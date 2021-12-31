c578822107b078d98687a66a136e138c50d0cfb7
AliBigdeli <bigdeli.ali3@gmail.com>
2021-12-31 09:37:55 +0330

    add docker-compose and enviroments

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,4 @@ COPY requirements.txt /app/
 RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
 
-COPY ./core /app
-
-CMD ["python3","manage.py","runserver","0.0.0.0:8000"]
+COPY ./core /app/
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!&hutf=af&s0n&9b6$nfq9-)%qwmdz!z)$)vubqppkm#+l%y_6'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default="*")
 
 
 # Application definition
