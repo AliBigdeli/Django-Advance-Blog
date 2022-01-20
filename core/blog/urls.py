5c2404ca00a1fbd93645312aef4f3a4a5e766e6d
AliBigdeli <bigdeli.ali3@gmail.com>
2022-01-20 17:51:04 +0330

    redirect view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import indexView
+from . import views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+app_name = "blog"
 
 urlpatterns = [
-    path('fbv-index', indexView,name="fbv-index"),
-    path('cbv-index', TemplateView.as_view(template_name="index.html",extra_context={"name":"ali"})),
+    path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
+    path('go-to-maktabkhooneh', views.RedirectToMaktab.as_view(), name='redirect-to-maktabkhooneh'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView,RedirectView
+from .models import Post
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+# Function Base View show a template
+''' 
 def indexView(request):
+    """
+    a function based view to show index page
+    """
     name = "ali"
     context = {"name":name}
     return render(request,"index.html",context)
+'''
+class IndexView(TemplateView):
+    """
+    a class based view to show index page
+    """
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context["name"] = "ali"
+        context["posts"] = Post.objects.all()
+        return context
+
+''' FBV for redirect
+from django.shortcuts import redirect
+def redirectToMaktab(request):
+    return redirect('https://maktabkhooneh.com')
+
+'''
+class RedirectToMaktab(RedirectView):
+    """
+    Redirection view sample for maktabkhooneh
+    """
+    url = 'https://maktabkhooneh.com'
+
+    def get_redirect_url(self, *args, **kwargs):
+        return super().get_redirect_url(*args, **kwargs)
+
