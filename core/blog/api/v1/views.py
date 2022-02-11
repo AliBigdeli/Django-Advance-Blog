71825139b8585dedc7087320a2ff2e7592e9bd55
AliBigdeli <bigdeli.ali3@gmail.com>
2022-02-11 16:30:57 +0330

    serializer many postlist

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
 from django.shortcuts import get_object_or_404
+
 
 @api_view()
 def postList(request):
-    return Response("ok")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts,many=True)
+    return Response(serializer.data)
 
 @api_view()
-def postDetail(request,id):
-    post = get_object_or_404(Post,pk=id)    
+def postDetail(request,id):  
+    post = get_object_or_404(Post,pk=id,status=True)    
     serializer = PostSerializer(post)    
     return Response(serializer.data)
+
     
