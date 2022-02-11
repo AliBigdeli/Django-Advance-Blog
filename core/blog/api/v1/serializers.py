7536770d5d8d5f5355fc79bf6a444b41843a4628
AliBigdeli <bigdeli.ali3@gmail.com>
2022-02-11 17:05:55 +0330

    aadding post request to list

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from ...models import Post
 
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id','author','title','content','category','status','created_date','published_date']
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,11 +6,18 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 
-@api_view()
+@api_view(["GET","POST"])
 def postList(request):
-    posts = Post.objects.filter(status=True)
-    serializer = PostSerializer(posts,many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts,many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
 
 @api_view()
 def postDetail(request,id):  
