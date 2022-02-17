ae6a76413c05a60cc9b421055e39ec0d5dec1001
AliBigdeli <bigdeli.ali3@gmail.com>
2022-02-17 15:58:21 +0330

    adding auto detect user to post creation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from ...models import Post,Category
-
+from accounts.models import Profile
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
@@ -9,6 +9,7 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','name']
+    
 
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
@@ -16,16 +17,30 @@ class PostSerializer(serializers.ModelSerializer):
     absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
     class Meta:
         model = Post
-        fields = ['id','author','title','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+        fields = ['id','author','image','title','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+        read_only_fields = ['author']
     
     def get_abs_url(self,obj):
         request = self.context.get('request')        
         return request.build_absolute_uri(obj.pk)
 
     def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep['category'] = CategorySerializer(instance.category).data
-        rep.pop('snippet',None)
+        request = self.context.get('request')
+        rep = super().to_representation(instance)        
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet',None)
+            rep.pop('relative_url',None)
+            rep.pop('absolute_url',None)
+        else:
+            rep.pop('content',None)
+        rep['category'] = CategorySerializer(instance.category,context={'request':request}).data
         return rep
+
+    def create(self, validated_data):
+        validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+        return super().create(validated_data)
+
+    
+    
         
 
