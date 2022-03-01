41b2ec61e19c33de7c94b360b0e0814d0e1561a2
AliBigdeli <bigdeli.ali3@gmail.com>
2022-03-01 12:04:21 +0330

    adding api documentation swagger and redoc

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'blog',
     'rest_framework',
     'django_filters',
+    'drf_yasg'
     
 ]
 
--- core/core/urls.py
+++ core/core/urls.py
@@ -18,13 +18,32 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog Api",
+      default_version='v1',
+      description="this is a test api for maktabkhooneh project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="bigdeli.ali3@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('blog/', include('blog.urls')),
-    path('api-docs/',include_docs_urls(title='api sample'))
+    path('api-docs/',include_docs_urls(title='api sample')),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # serving static and media for development
