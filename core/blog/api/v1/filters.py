58ca8bed8205cec9bee1cbd2803ac25aff729d08
AliBigdeli <bigdeli.ali3@gmail.com>
2022-03-01 11:08:58 +0330

    filters

---
+++ core/blog/api/v1/filters.py
@@ -0,0 +1,9 @@
+from django_filters import rest_framework as filters
+from ...models import Post
+
+
+class PostFilters(filters.FilterSet):
+
+    class Meta:
+        model = Post
+        fields = {'category':["exact","in"], 'author':["exact"],'status':["exact"]}
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -11,6 +11,7 @@ from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter,OrderingFilter
 from .paginations import DefaultPagination
+from .filters import PostFilters
 # Example for Function Based View
 """
 from rest_framework.decorators import api_view,permission_classes
@@ -116,7 +117,8 @@ class PostModelViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
-    filterset_fields = ['category', 'author','status']
+    # filterset_fields = {'category':["exact","in"], 'author':["exact"],'status':["exact"]}
+    filterset_class = PostFilters
     search_fields = ['title', 'content']
     ordering_fields = ['published_date']
     pagination_class = DefaultPagination
