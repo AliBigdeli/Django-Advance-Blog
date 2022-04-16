bcc24b1ac190af17c3baf08fe3ab38e93a7e1c90
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-04-16 06:30:45 +0400

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'..
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
