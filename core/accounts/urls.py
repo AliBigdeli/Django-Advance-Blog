5d1b0ab453d99039a99996ffd4a9d6314b399a74
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-05-09 23:48:20 +0400

    added sample shared task

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-
+from . import views
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("send-email/", views.send_email, name="send-email"),
     # path('api/v1/',include('accounts.api.v1.urls')),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+import time 
+from .tasks import sendEmail
 
-# Create your views here.
+
+def send_email(request):
+    sendEmail.delay()
+    return HttpResponse("<h1>Done Sending</h1>")
